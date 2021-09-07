b55f43dfefbb8c38afa0c55e90c21cb68f0087f9
Andrade <luigipaulo.desenvolvedor@gmail.com>
2021-09-07 00:49:55 -0300

    Classes DataBase e Validator criadas nos respectivos módulos database.py e validator.py

---
+++ models/database.py
@@ -0,0 +1,4 @@
+class DataBase:
+    """Classe que representará o banco de dados"""
+    def __init__(self):
+        pass
---
+++ models/validator.py
@@ -0,0 +1,4 @@
+class Validator:
+    """Classe responsável por validar as informações no banco de dados."""
+    def __init__(self):
+        pass
