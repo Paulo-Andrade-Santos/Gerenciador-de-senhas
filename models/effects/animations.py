9fd9b7f1d02e41d6c4705d544c98a14859ef02d3
Andrade <luigipaulo.desenvolvedor@gmail.com>
2021-09-08 16:30:37 -0300

    Tela de boas-vindas finalizada com sucesso

---
+++ models/effects/animations.py
@@ -0,0 +1,54 @@
+from tkinter import (Tk, StringVar, Label)
+from webbrowser import open
+
+
+class Animations:
+    def __init__(self, root: Tk, stringvar: StringVar, lb_github: Label) -> None:
+        self.__root: Tk = root
+        self.__stringvar: StringVar = stringvar
+        self.__text: str = "Antes de armazenar as suas senhas, crie um perfil para ter mais segurança"
+        self.__github: Label = lb_github
+        self.__counter: int = 0
+        self.automatic_writing()
+
+    def automatic_writing(self):
+        """Método responsável por criar uma animação de escrita na tela inicial do programa."""
+        if self.__counter <= len(self.__text):
+            if self.__counter == len(self.__text):
+                self.__counter = 0
+                self.give_me_attention()
+            else:
+                self.__stringvar.set(self.__stringvar.get() + self.__text[self.__counter])
+                self.__counter += 1
+                self.__root.after(50, self.automatic_writing)  # Velocidade da escrita automática.
+
+    def give_me_attention(self) -> None:
+        """Método responsável por realizar a animação de 'pisca-pisca' no rodapé da tela principal."""
+
+        def blue_light(label: Label) -> None:
+            """Função responsável por alterar a cor do texto para azul."""
+            if self.__counter <= 5:
+                label.configure(foreground="#0000FF")
+                self.__root.after(100, lambda: orange_light(label))
+            else:
+                label.configure(foreground="#0000FF")
+
+        def orange_light(label: Label) -> None:
+            """Função responsável por alterar a cor do texto para laranja."""
+            label.configure(foreground="#FFA500")
+            self.__counter += 1
+            self.__root.after(100, lambda: blue_light(label))
+        blue_light(self.__github)
+
+    @staticmethod
+    def change_cursor(lb_github: Label, status: int):
+        """Método estático responsável por alterar o cursor quando ele estiver sobre o rodapé da tela principal."""
+        if status == 1:
+            lb_github.configure(cursor="hand2", font=("Segoe UI Black", 10, "underline"))
+        else:
+            lb_github.configure(cursor="arrow", font=("Segoe UI Black", 10))
+
+    @staticmethod
+    def redirect(event):
+        """Método estático responsável por direcionar o usuário para o github"""
+        open("https://github.com/Paulo-Andrade-Santos?tab=repositories")
--- models/user_interface/welcome_screen.py
+++ models/user_interface/welcome_screen.py
@@ -2,17 +2,20 @@ from tkinter import (
     Tk,
     Frame,
     Label,
-    Entry,
-    Button
+    Button,
+    PhotoImage,
+    StringVar,
+    LEFT
 )
+from models.effects.animations import Animations
 
 
 class WelcomeScreen:
     """Classe resposável por exibir a janela inicial do programa."""
     def __init__(self) -> None:
         """Construtor da classe. Responsável por atribuir as características da janela principal (__main_window)."""
-        self.__main_window = Tk()
-        self.__main_window.title("Gerenciador de senhas")
+        self.__main_window: Tk = Tk()
+        self.__main_window.title("SinglePass")
         self.__main_window.iconbitmap("icons/key_icon_2.ico")
         self.__main_window.resizable(width=0, height=0)
         self.__main_window.geometry(newGeometry="%dx%d+%d+%d" % (
@@ -21,7 +24,58 @@ class WelcomeScreen:
             self.__main_window.winfo_screenwidth() / 2 - (self.__main_window.winfo_screenwidth() / 2) / 2,
             self.__main_window.winfo_screenheight() / 2 - (self.__main_window.winfo_screenheight() / 2 + 100) / 2
         ))
-        self.execute()
+
+        # Variáveis dinâmicas:
+        self.__automatic_writing: StringVar = StringVar()
+
+        # Fotos que serão utilizadas no programa:
+        self.__one_key: PhotoImage = PhotoImage(file="images/keys_02_72px.png")
+        self.__new_profile: PhotoImage = PhotoImage(file="images/new_profile_02_32px.png")
+        self.__remove_profile: PhotoImage = PhotoImage(file="images/remove_profile_02_32px.png")
+        self.__exit: PhotoImage = PhotoImage(file="images/exit_01.png")
+
+        # Chamando todas as funções responsáveis por desenhar os ítens necessários na tela:
+        self.__make_frames()
+        self.__make_labels()
+        self.__make_buttons()
+
+        # Chamando as animações da tela:
+        self.__effects()
+
+    def __make_frames(self) -> None:
+        """Método responsável por desenhar as molduras (frames)."""
+        self.__top_frame: Frame = Frame(master=self.__main_window)
+        self.__top_frame.configure(relief="solid", borderwidth=1, width=663, height=170)
+        self.__top_frame.grid(row=0, column=0, padx=10, pady=10)
+
+        self.__bottom_frame: Frame = Frame(master=self.__main_window)
+        self.__bottom_frame.configure(relief="solid", borderwidth=1, width=663, height=160)
+        self.__bottom_frame.grid(row=1, column=0, padx=10, pady=5)
+
+    def __make_labels(self) -> None:
+        """Método responsável por desenhar os rótulos (labels)."""
+        Label(master=self.__top_frame, image=self.__one_key).place(x=280, y=20)
+        Label(master=self.__top_frame, text="Seja bem-vindo ao SinglePass", font=("Corbel Bold", 15, "underline"), foreground="#DAA520").place(x=190, y=100)
+        self.__animated_label: Label = Label(master=self.__top_frame)
+        self.__animated_label.configure(font=("Ink Free", 15), textvariable=self.__automatic_writing, foreground="#000080")
+        self.__animated_label.place(x=6, y=130)
+        self.__created_by: Label = Label(master=self.__main_window, text="Criado por: Paulo Andrade", foreground="#4F4F4F", font=("Segoe UI Black", 10))
+        self.__created_by.place(x=7, y=356)
+        self.__github: Label = Label(master=self.__main_window, text="Meu Github", foreground="#363636", font=("Segoe UI Black", 10))
+        self.__github.bind("<Enter>", lambda event: Animations.change_cursor(self.__github, 1))
+        self.__github.bind("<Leave>", lambda event: Animations.change_cursor(self.__github, 0))
+        self.__github.bind("<Button-1>", Animations.redirect)
+        self.__github.place(x=595, y=356)
+
+    def __make_buttons(self) -> None:
+        """Método responsável por desenhar os botões (buttons)."""
+        Button(master=self.__bottom_frame, image=self.__new_profile, text="Criar Perfil", font=("Ink Free", 15, "bold"), compound=LEFT, width=170).place(x=230, y=10)
+        Button(master=self.__bottom_frame, image=self.__remove_profile, text="Remover Perfil", font=("Ink Free", 15, "bold"), compound=LEFT, width=190).place(x=220, y=60)
+        Button(master=self.__bottom_frame, image=self.__exit, text="Sair", font=("Ink Free", 15, "bold"), compound=LEFT, width=100, command=lambda: exit(0)).place(x=265, y=110)
+
+    def __effects(self):
+        """Método responsável por chamar todas as animações que ocorrem na tela principal"""
+        Animations(self.__main_window, self.__automatic_writing, self.__github)
 
     def execute(self) -> None:
         """Método responsável por iniciar a execução da janela principal."""
